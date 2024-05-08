db043e8e474985764f30888a79aea0697e165705
AnikaPuri <42912590+AnikaPuri@users.noreply.github.com>
2024-05-08 13:03:34 -0400

    commit changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Lab 8: Map</h1>
+<p>Paragraph Description of Lab 8</p>
+
+
