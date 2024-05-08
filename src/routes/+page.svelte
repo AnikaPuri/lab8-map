d42aa9aebffd4868274dc57f98f494ea29bf4195
AnikaPuri <42912590+AnikaPuri@users.noreply.github.com>
2024-05-08 16:46:40 -0400

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
-<style>
-    @import url("$lib/global.css");
-</style>
+
 <h1>Lab 8: Map</h1>
 <p>Paragraph Description of Lab 8</p>
 
+<div id="map" />
 
+
+<style>
+
+@import url("$lib/global.css");
+
+
+#map {
+	flex: 1;
+    color:aqua
+}
+
+</style>
