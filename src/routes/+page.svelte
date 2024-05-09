3b97a54e8e70a1babfecac28344817cd9ded2076
AnikaPuri <42912590+AnikaPuri@users.noreply.github.com>
2024-05-09 13:38:51 -0400

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,45 @@
 <script>
+    import { onMount } from "svelte";
+
     import mapboxgl from "mapbox-gl";
     import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
     mapboxgl.accessToken = "pk.eyJ1IjoiYW5wdXJpIiwiYSI6ImNsdmlrZ2hraTAwdnQyanA5ano1aTRrdTcifQ.jnicnrkT06m-Bb2x1IwTEw";
+
+    onMount(async () => {
+        // Create the map
+        let map = new mapboxgl.Map({
+            container: "map", // The ID of the HTML element that contains the map
+            style: "mapbox://styles/mapbox/streets-v12", // Mapbox style URL
+            center: [-71.0942, 42.3601], // Longitude and latitude (MIT HQ coordinates)
+            zoom: 12 // Initial zoom level
+        });
+
+        // Wait for the map to load before adding the source
+        await new Promise((resolve) => map.on("load", resolve));
+
+        // Add the GeoJSON data source
+        map.addSource("boston_route", {
+            type: "geojson",
+            data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network.geojson"
+        });
+
+        // Add a layer to visualize the data (e.g., line layer for the route)
+        map.addLayer({
+            id: "boston_route_layer",
+            type: "line",
+            source: "boston_route",
+            paint: {
+                "line-color": "#888",
+                "line-width": 3
+            }
+        });
+
+        // Optional: Add navigation controls to the map (zoom buttons)
+        //map.addControl(new mapboxgl.NavigationControl());
+    });
+
 </script>
+
 
 
 <h1>Lab 8: Map</h1>
